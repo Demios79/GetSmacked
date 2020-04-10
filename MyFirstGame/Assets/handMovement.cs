e9d2c60450449c9233fe4b43345ab6fb24f1bfc7
Jose Gomez <demigomez79@gmail.com>
2020-04-09 23:06:14 -0700

    Just Some Changes To the Feel of Physics
    - Edited Player Movement
    - Need to Fix: Hand model, resetPoint Collider
    -Added test level scence in project using probuilder

--- MyFirstGame/Assets/handMovement.cs
+++ MyFirstGame/Assets/handMovement.cs
@@ -12,7 +12,8 @@ public class handMovement : MonoBehaviour
     public Collider resetPoint;
     public float reloadSpeed;
     public float returnSpeed;
-    Vector3 origin;
+    public int timeMulti;
+    private Vector3 origin;
     [SerializeField]
     private float currentSpeed;
     [SerializeField]
@@ -22,6 +23,8 @@ public class handMovement : MonoBehaviour
     [SerializeField]
     private bool returning;
     private bool canCharge;
+    private float accel;
+    private float time;
 
     // Start is called before the first frame update
     void Start()
@@ -31,10 +34,11 @@ public class handMovement : MonoBehaviour
         atOrigin = true;
         returning = false;
         canCharge = true;
+        time = 0;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         hand.RotateAround(cam.transform.position, cam.transform.up, currentSpeed);
         origin = resetPoint.transform.position;
@@ -44,7 +48,8 @@ public class handMovement : MonoBehaviour
 
             if(!rotating && canCharge && !returning)
             {
-                currentSpeed = reloadSpeed;
+                time += Time.deltaTime;
+                currentSpeed = (reloadSpeed) * time * timeMulti;
                 material.SetColor("_Color",color);
                 rotating = true;
                 atOrigin = false;
@@ -55,6 +60,7 @@ public class handMovement : MonoBehaviour
             {
                 currentSpeed = 0;
                 rotating = false;
+                time = 0;
             }
         
         }
@@ -63,9 +69,10 @@ public class handMovement : MonoBehaviour
         {
             if((!returning && rotating) || (!returning && atOrigin))
             {
+                time += Time.deltaTime;
                 atOrigin = false;
                 material.SetColor("_Color",returnColor);
-                currentSpeed = returnSpeed;
+                currentSpeed = (returnSpeed) * time * timeMulti;
                 returning = true;
                 
             }
--- MyFirstGame/Assets/playerMovement.cs
+++ MyFirstGame/Assets/playerMovement.cs
@@ -6,11 +6,11 @@ public class playerMovement : MonoBehaviour
 {
 
     public CharacterController controller;
-    public float jumpHeight = 5f;
-    public float speed = 12f;
-    public float gravity = -10f;
+    public float jumpHeight;
+    public float speed;
+    public float gravity;
     public Transform groundCheck;
-    public float groundDistance = 0.4f;
+    public float groundDistance = 0.8f; // Radius for GroundCheck physics sphere
     public LayerMask groundMask;
     Vector3 velocity;
     bool isGrounded;
@@ -24,25 +24,35 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Checks the the Player is touching ground every update
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        // Make sure the player is on the ground
         if(isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f;
+            velocity.y = 0f; // forces player on ground
         }
+        
+        // Gets Input for x and z Directions
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        // Make Input into a vector
         Vector3 move = transform.right * x + transform.forward *z;
 
+        // Moves Contoller using Move Vector multiplied by speed (Time is added to make independent of framerate)
+        controller.Move(move * speed * Time.deltaTime);
+        
+        // If Jump button is pressed then calulate vecolity needed to reach Jump Height
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity );
         }
 
-        controller.Move(move * speed * Time.deltaTime);
-
-        velocity.y += gravity * Time.deltaTime;
-
+        // Moves Controller in Jump Direction (+y) accord to Velocity
         controller.Move(velocity * Time.deltaTime);
+
+        // Applies "Gravity" to the y Velocity
+        velocity.y += gravity * Time.deltaTime;
     }
 }

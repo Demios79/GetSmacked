9f21a23f0125afd179d83541bd072f732fd74dda
Agustin Gomez <gusmandoman@gmail.com>
2020-04-12 22:27:38 -0700

    FixedHand

--- MyFirstGame/Assets/handMovement.cs
+++ MyFirstGame/Assets/handMovement.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -27,7 +27,7 @@ public class handMovement : MonoBehaviour
     private float accel;
     private float time;
 
-    // Start is called before the first frame update
+    // Initial State without any changes
     void Start()
     {
         currentSpeed = 0;
@@ -41,24 +41,38 @@ public class handMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Constantly rotating depends on currentSpeed variable;
         hand.RotateAround(cam.transform.position, cam.transform.up, currentSpeed);
+
+        //Constantly updating the origin as the player is able to look around
         origin = resetPoint.transform.position;
 
+        //All states surrounding a held mouse button
         if(Input.GetMouseButton(0))
         {
-
+            //If the player is able to charge, a hit
+            //this stage runs
             if(!rotating && canCharge && !returning)
             {
+                //Timer starts here for a charge and multiplier
                 time += Time.deltaTime;
+                //Setting a velocity calculated by time held and 
+                //a multiplier set in editor
                 currentSpeed = (reloadSpeed) * time * timeMulti;
+                //Color Change
                 material.SetColor("_Color",color);
+                //State variables set
                 rotating = true;
                 atOrigin = false;
                 canCharge = false;
             }
 
+            //If the player hits the origin point while rotating
             if(atOrigin && rotating)
             {
+                //The speed is set to 0
+                //and the object stops rotating
+                //time multiplier reset
                 currentSpeed = 0;
                 rotating = false;
                 time = 0;
@@ -66,25 +80,36 @@ public class handMovement : MonoBehaviour
         
         }
 
+        //This if statement will happen in reaction to releasing
+        //the mouse button.
         if(!Input.GetMouseButton(0) && !canCharge)
         {
+            //This wil begin returning the hand to the origin in the
+            //reverse direction
             if((!returning && rotating) || (!returning && atOrigin))
             {
+                //Time starts as return begins 
                 time += Time.deltaTime;
+                //As movement begins the hand is no longer at origin,
+                //the color changes and the  speed is set to a 
+                //negative value
                 atOrigin = false;
                 material.SetColor("_Color",returnColor);
                 currentSpeed = (returnSpeed) * time * timeMulti;
+                //The value is now returning but the value is not rotating
                 returning = true;
                 rotating = false;
                 
             }
 
-            else if(returning && atOrigin)
+            //This will happen when the vvalue hits the original 
+            //origin point, comming to a stop and changing approproate variables;
+            else if(returning && atOrigin && !canCharge)
             {
-                canCharge = true;
                 returning = false;
                 time = 0;
                 currentSpeed = 0;
+                canCharge = true;
             }
         }
 
